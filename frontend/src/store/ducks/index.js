85cbfe63d80b5c6a097a9b79f19c679f7f80e12b
Marcus Paulo <mpaulobr@gmail.com>
2019-10-21 23:37:03 -0300

    Ajustes na implementação

--- frontend/src/store/ducks/index.js
+++ frontend/src/store/ducks/index.js
@@ -1,5 +1,12 @@
 import { combineReducers } from 'redux';
+import { connectRouter } from 'connected-react-router';
 
-export default combineReducers({
-  test: () => [],
-});
+import { reducer as toastr } from 'react-redux-toastr';
+import { reducer as auth } from './auth';
+
+export default history =>
+  combineReducers({
+    auth,
+    toastr,
+    router: connectRouter(history),
+  });
--- frontend/src/store/sagas/index.js
+++ frontend/src/store/sagas/index.js
@@ -1,5 +1,11 @@
-import { all } from 'redux-saga/effects';
+import { all, takeLatest } from 'redux-saga/effects';
+
+import { signIn, signOut } from './auth';
+import { AuthTypes } from '../ducks/auth';
 
 export default function* rootSaga() {
-  return all([]);
+  return yield all([
+    takeLatest(AuthTypes.SIGN_IN_REQUEST, signIn),
+    takeLatest(AuthTypes.SIGN_OUT, signOut),
+  ]);
 }
