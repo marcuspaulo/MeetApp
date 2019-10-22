3da3039dce8e3be151a20233e12ad68415678224
Marcus Paulo <mpaulobr@gmail.com>
2019-10-22 00:32:35 -0300

    Configuração do Reactotron

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { Router } from 'react-router-dom';
 import Routes from './routes';
 
+import './config/ReactotronConfig';
+
 import history from './services/history';
 
 function App() {
