25ef037bd5621673a1bfef967b3c7b37c880afb4
Marcus Paulo <mpaulobr@gmail.com>
2019-10-23 21:31:54 -0300

    Implementação da estilização da página de login e registro, além da validação

--- frontend/src/pages/SignIn/index.js
+++ frontend/src/pages/SignIn/index.js
@@ -1,7 +1,35 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { Form, Input } from '@rocketseat/unform';
+import * as Yup from 'yup';
 
-// import { Container } from './styles';
+import logo from '~/assets/logo.svg';
+
+const schema = Yup.object().shape({
+  email: Yup.string()
+    .email('Insira um e-mail válido')
+    .required('O e-mail é obrigatório'),
+  password: Yup.string()
+    .min(6, 'No mínimo 6 caracteres')
+    .required('A senha é obrigatória.'),
+});
 
 export default function SignIn() {
-  return <h1>Login</h1>;
+  function handleSubmit(data) {
+    console.tron.log(data);
+  }
+
+  return (
+    <>
+      <img src={logo} alt="Logo MeetApp" />
+
+      <Form schema={schema} onSubmit={handleSubmit}>
+        <Input name="email" type="email" placeholder="Digite seu e-mail" />
+        <Input name="password" type="password" placeholder="Digite sua senha" />
+
+        <button type="submit">Acessar</button>
+        <Link to="/register">Criar sua conta gratuita</Link>
+      </Form>
+    </>
+  );
 }
