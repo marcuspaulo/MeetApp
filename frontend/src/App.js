e8873a69cf0046870168e4fa11a28907e0da0aa2
Marcus Paulo <mpaulobr@gmail.com>
2019-10-23 23:49:24 -0300

    Implementação do Login do Usuário chamando a API

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import { Router } from 'react-router-dom';
-import Routes from './routes';
 
 import './config/ReactotronConfig';
 
+import Routes from './routes';
 import history from './services/history';
+
 import store from './store';
 
 import GlobalStyle from './styles/global';
